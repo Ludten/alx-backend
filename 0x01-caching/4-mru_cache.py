6a7eca6710eca6ad9aa1acdc41c2e900e1410283
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-01-25 19:05:29 +0100

    Caching
    MRU and more redundacy removal

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """
-LRUCache module
+MRUCache module
 """
 
 from datetime import datetime
 from base_caching import BaseCaching
 
 
-class LRUCache(BaseCaching):
+class MRUCache(BaseCaching):
     """
-    LRU Cache class
+    MRU Cache class
     """
 
     def __init__(self):
@@ -21,6 +21,21 @@ class LRUCache(BaseCaching):
         Cache an item
         """
         if key is not None and item is not None:
+            if (len(self.cache_data) == BaseCaching.MAX_ITEMS):
+                if key not in self.cache_data:
+                    MRU_key = {k: v
+                               for k, v in self._keys.items()
+                               if v["val"] == min(self._keys.values(),
+                                                  key=lambda x: x[
+                                   'val'])["val"]}
+                    keyd = [k for k, v in MRU_key.items()
+                            if v["time"] == max(MRU_key.values(),
+                                                key=lambda x: x[
+                                                    'time'])["time"]]
+                    self._keys.pop(keyd[0])
+                    self.cache_data.pop(keyd[0])
+                    print("DISCARD: {}".format(keyd[0]))
+
             if key in self.cache_data:
                 self._keys[key] = {"val": 0, "time": datetime.now()}
                 self.cache_data[key] = item
@@ -29,17 +44,6 @@ class LRUCache(BaseCaching):
                     self._keys[k]["val"] += 1
                 self._keys[key] = {"val": 0, "time": datetime.now()}
                 self.cache_data[key] = item
-            # print(self._keys)
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                lru_key = {k: v for k, v in self._keys.items()
-                           if v["val"] == max(self._keys.values(),
-                           key=lambda x: x['val'])["val"]}
-                keyd = [k for k, v in lru_key.items()
-                        if v["time"] == min(lru_key.values(),
-                                            key=lambda x: x['time'])["time"]]
-                self._keys.pop(keyd[0])
-                self.cache_data.pop(keyd[0])
-                print("DISCARD: {}".format(keyd[0]))
 
     def get(self, key):
         """
