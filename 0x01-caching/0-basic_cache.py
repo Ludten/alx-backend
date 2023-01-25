df3913b67fbb36a1b09f08fdf8d8f1837767fff4
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-01-25 15:47:37 +0100

    Caching
    Base cache class, basic cache class and directory readme

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+BasicCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Basic Cache class
+    """
+
+    def put(self, key, item):
+        """
+        Cache an item
+        """
+        if key or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieve an item
+        """
+        return self.cache_data[key]
