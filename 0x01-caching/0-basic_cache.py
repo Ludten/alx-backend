19bf53a8ad299e0aae13cd1e99591d7aa532c334
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-01-25 16:00:11 +0100

    Caching
    variable annotation check

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -3,7 +3,7 @@
 BasicCache module
 """
 
-from typing import Union
+from typing import Any, Union
 from base_caching import BaseCaching
 
 
@@ -12,17 +12,14 @@ class BasicCache(BaseCaching):
     Basic Cache class
     """
 
-    def put(
-        self, key: Union[int, bytes, str, float],
-        item: Union[int, bytes, str, float]
-    ) -> None:
+    def put(self, key: Any, item: Any) -> None:
         """
         Cache an item
         """
         if key or item is not None:
             self.cache_data[key] = item
 
-    def get(self, key):
+    def get(self, key: Any) -> Union[Any, None]:
         """
         Retrieve an item
         """
