db19d89c7d51927fe0a75c72ca31036356e9d5c6
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-01-31 21:58:59 +0100

    I18n
    wrong html

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -35,7 +35,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index():
     """returns template"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
