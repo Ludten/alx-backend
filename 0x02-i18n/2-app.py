91d2c7da29e127754e6588a02385c184d2f6357a
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-01-31 16:28:24 +0100

    I18n
    2-app

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+starts a Flask web application
+"""
+
+from flask import Flask, request, render_template
+from flask_babel import Babel
+
+
+class Config():
+    """
+    Languages config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Selecte best locale selector
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns template"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
